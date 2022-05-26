67aa70184396895cf9aa662005b01afc97ba8320
Mohamad hosein dehghani <de_mh@yahoo.com>
2022-05-26 18:03:01 +0430

    added ability to mount eraab on word and a function to validate if word is pronouncable

--- dataset/process.py
+++ dataset/process.py
@@ -30,8 +30,62 @@ def process_entry(file):
 def extract_eraab(word, description):
     eraab_group = re.search(r'^\[([^\]]*)\]', description)
     # assert (len(eraab_group.groups()) == 1)
-    eraab = eraab_group.group(1) if eraab_group is not None else eraab_group
-    return eraab
+    eraab = eraab_group.group(1).strip() if eraab_group is not None else eraab_group
+    if eraab == '':
+        eraab = None
+
+    if eraab is not None:
+        eraab_list = eraab.strip().replace('ء', 'ئ').split(' ')
+        transformed_word = ''
+        word_idx = 0
+        eraab_idx = 0
+        while eraab_idx < len(eraab_list):
+            if word_idx >= len(word):
+                print(f'erorr parsing {word} with {eraab}')
+                return word
+
+            if eraab_list[eraab_idx] == '/':
+                eraab_idx += 2
+                continue
+            transformed_word += word[word_idx]
+
+            if eraab_list[eraab_idx][0] == word[word_idx]:
+                if len(eraab_list[eraab_idx]) == 2 and eraab_list[eraab_idx][1] in 'َُِْ':
+                    transformed_word += eraab_list[eraab_idx][1]
+                elif len(eraab_list[eraab_idx]) == 2 and eraab_list[eraab_idx][1] == word[word_idx + 1]:
+                    pass
+                else:
+                    print(f'wrong eraab {eraab} for word {word}')
+                    return word
+                eraab_idx += 1
+
+            word_idx += 1
+        if word_idx < len(word):
+            transformed_word = (transformed_word + word[word_idx:]) 
+    return transformed_word if eraab is not None else word
+
+
+def validate_pronounce(eraab):
+    #TODO: add characters that allow 3 samet.
+    samet = 3
+    start = True
+    for ch in eraab:
+        if start:
+            start = False
+            if ch == 'آ':
+                samet = 0
+            continue
+        if samet == 0 and ch not in "اَُِآ":
+            samet += 1
+            continue
+        if ch in 'آاَوُیِ':
+            samet = 0
+        else:
+            samet += 1
+        if samet > 3:
+            return False
+    return True if samet < 3 else False
+
 
 def extract_pos(description):
     pos_group = re.search(r'<span style="color:orange">\((.*)\)</span>', description)
@@ -71,16 +125,21 @@ class Database:
 if __name__ == "__main__":
 #iterate over folders
     start_time = time.time()
-
+    # counter = 1 
     db = Database()
     for i in range(35):
         for file in os.listdir(f'dehkhoda/{i}/'):
-            # print(file)
+            # print(counter)
+            # counter += 1
             word, description = process_entry(f'dehkhoda/{i}/{file}')
+            if ' ' in word or '‌' in word: #space or half space
+                continue
             if word == None or word == '':
                 print(f'{file} is empty')
             pos = extract_pos(description)
             eraab = extract_eraab(word, description)
+            if not validate_pronounce(eraab):
+                print(f'eraab {eraab} is not pronouncable')
             # meaning = extract_meaning(description)
             meaning = description
             IPA = None

f55d02315d10adc8351a33aa7003406445a9ff88
MohamadHosein Dehghani <de_mh@yahoo.com>
2022-06-06 19:56:05 +0430

    add test for eraab Phonemization

--- tests/test_phonemizer.py
+++ tests/test_phonemizer.py
@@ -3,9 +3,16 @@ from persian_phonemizer import Phonemizer
 
 
 class TestPhonemizer(unittest.TestCase):
-    def test_Phonemizer(self):
+    def test_Phonemizer_IPA(self):
         phonemizer = Phonemizer()
         cases = [("آرادان", "ʔɒːɾɒːdɒːn"),
          ("این یک متن فارسی است.", "ʔiːn jækʰ mætʰn fɒːɾsiː ʔæstʰ .")]
+        for case in cases:
+            self.assertEqual(phonemizer.phonemize(case[0]), case[1])
+
+    def test_Phonemizer_eraab(self):
+        phonemizer = Phonemizer(output_format="eraab")
+        cases = [("آرادان", "آرادان"), ("سلام", "سَلام"), 
+         ("این متن فارسی است.", "این مَتن فارسی اَست .")]
         for case in cases:
             self.assertEqual(phonemizer.phonemize(case[0]), case[1])

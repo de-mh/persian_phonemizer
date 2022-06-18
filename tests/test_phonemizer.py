e3486cde4490eff8286a86acb767c1a23727c8ff
MohamadHosein Dehghani <de_mh@yahoo.com>
2022-06-18 14:12:30 +0430

    bugfix

--- tests/test_phonemizer.py
+++ tests/test_phonemizer.py
@@ -12,7 +12,7 @@ class TestPhonemizer(unittest.TestCase):
 
     def test_Phonemizer_eraab(self):
         phonemizer = Phonemizer(output_format="eraab")
-        cases = [("آرادان", "آرادان"), ("سلام", "سَلام"), 
+        cases = [("فارسی", "فارسی"), ("سلام", "سَلام"), 
          ("این متن فارسی است.", "این مَتن فارسی اَست .")]
         for case in cases:
             self.assertEqual(phonemizer.phonemize(case[0]), case[1])
